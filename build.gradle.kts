a64d121228f38e8535a10283f2e58b127be57109
steventruong1 <97932227+steventruong1@users.noreply.github.com>
2023-11-01 23:26:55 -0400

    Added Boilerplate for Plugin Development
    Opens up latest Pycharm version. Check tools and there should be a new option called Pop Action Dialog. Based off this https://github.com/JetBrains/intellij-sdk-code-samples/tree/main/product_specific/pycharm_basics and updated for our uses.

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    id("java")
+    id("org.jetbrains.intellij") version "1.15.0"
+}
+
+group = "org.jetbrains.research.anticopypasterpython"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+// Configure Gradle IntelliJ Plugin
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
+intellij {
+    version.set("2023.2.3")
+    type.set("PY")
+    plugins.set(listOf("Pythonid"))
+    downloadSources.set(false)
+}
+
+tasks {
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+
+    buildSearchableOptions {
+        enabled = false
+    }
+
+    patchPluginXml {
+        sinceBuild.set("222")
+        untilBuild.set("232.*")
+    }
+
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+
+    publishPlugin {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+}

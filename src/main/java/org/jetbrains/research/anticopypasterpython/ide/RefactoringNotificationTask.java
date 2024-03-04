6f771c21d63fd99276aaebf64b54c71e19adfafc
cphilli4 <cphilli4@stevens.edu>
2024-03-04 15:41:36 -0500

    Minor fixes

--- src/main/java/org/jetbrains/research/anticopypasterpython/ide/RefactoringNotificationTask.java
+++ src/main/java/org/jetbrains/research/anticopypasterpython/ide/RefactoringNotificationTask.java
@@ -286,7 +286,7 @@ public class RefactoringNotificationTask extends TimerTask {
                     float prediction = model.predict(featuresVector);
                     System.out.println("Prediction: " + prediction);
                     System.out.println("Threshold: " + predictionThreshold);
-                    prediction = 999999999;
+//                    prediction = 999999999;
 //                    if (debugMetrics) {
 //                        UserSettingsModel settingsModel = (UserSettingsModel) model;
 //                        try (FileWriter fr = new FileWriter(logFilePath, true)) {
